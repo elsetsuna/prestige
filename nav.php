b793065cf0d9b0619bf1f4033e0c84702783f8f9
elsetsuna <vnz_inside@vadine.net>
2015-06-10 15:29:44 +0700

    Fix navigation bar menu

--- nav.php
+++ nav.php
@@ -30,21 +30,9 @@ echo '
         <li><a href="index.php"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
         <li><a href="?module=tprestige"> <i class="icon-credit-card"></i><span>Taipan Prestige</span></a>
         </li>
-        <li><a href="guidely.html"><i class="icon-facetime-video"></i><span>App Tour</span> </a></li>
-        <li><a href="charts.html"><i class="icon-bar-chart"></i><span>Charts</span> </a> </li>
-        <li><a href="shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
-        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Drops</span> <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="icons.html">Icons</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="pricing.html">Pricing Plans</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="signup.html">Signup</a></li>
-            <li><a href="error.html">404</a></li>
-          </ul>
-        </li>
+        <li><a href="#"><i class="icon-group"></i><span>Membership</span> </a></li>
+        <li><a href=""><i class="icon-gift "></i><span>Rewards</span> </a> </li>
         <li ><a href="?module=card"><i class="icon-credit-card"></i><span>Cards</span></a></li>
-         <li><a href="?module=setrate"><i class="icon-money"></i><span>Set Rate</span> </a> </li>
       </ul>
     </div>
     <!-- /container --> 
